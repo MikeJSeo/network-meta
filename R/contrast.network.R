c296a66114a1537b40fc0bbc774edd59d8ccdc63
Michael Seo <swj8874@gmail.com>
2018-03-15 17:24:35 -0400

    revision

--- R/contrast.network.R
+++ R/contrast.network.R
@@ -48,8 +48,10 @@ contrast.network.data <- function(Outcomes, Treat, SE, na, V = NULL, type = "ran
   ntreat <- length(ntreat[!is.na(ntreat)])
   nstudy <- sum(na_count)
   
+  Treat.order <- unique(c(Treat))
+  Treat.order <- Treat.order[!is.na(Treat.order)]
   
-  network <- list(Outcomes = Outcomes, Treat = Treat, SE = SE, na = na, na_count = na_count, ntreat = ntreat, nstudy = nstudy, type = type, mean.d = mean.d, prec.d = prec.d, hy.prior = hy.prior, response = "normal")
+  network <- list(Outcomes = Outcomes, Treat = Treat, SE = SE, na = na, na_count = na_count, ntreat = ntreat, nstudy = nstudy, type = type, mean.d = mean.d, prec.d = prec.d, hy.prior = hy.prior, response = "normal", Treat.order = Treat.order)
   
   if(!is.null(V)){
     network$V <- V
