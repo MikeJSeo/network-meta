3455ca9b51f914f7cc349b3d000e233f0f0e7f3a
Michael Seo <swj8874@gmail.com>
2018-10-20 04:24:38 -0400

    revision

--- R/network.data.R
+++ R/network.data.R
@@ -146,8 +146,7 @@ network.data <- function(Outcomes, Study, Treat, N = NULL, SE = NULL, response =
         no_reference <- c(no_reference, i)  
       } 
     }
-    data <- rbind(data, no_reference)
-    print(data)
+    data <- rbind(data, fictitious.row(response, ncol, no_reference))
   }
   
   # permute the data so that base treatment arm is always listed first in each study
