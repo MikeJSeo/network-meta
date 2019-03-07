f55964692d6abfdb0161ae003a80e021eb04f500
Michael Seo <swj8874@gmail.com>
2019-03-07 18:50:54 +0100

    revision

--- R/nodesplit.network.R
+++ R/nodesplit.network.R
@@ -4,7 +4,7 @@
 #'
 #' @param Outcomes Arm-level outcomes. If it is a multinomial response, the matrix would be arms (row) by multinomial categories (column). If it is binomial or normal, it would be a vector.
 #' @param Study A vector of study indicator for each arm
-#' @param Treat A vector of treatment indicator for each arm. Treatments should have positive integer values starting from 1 to total number of treatments. In a study, lowest number is taken as the baseline treatment.
+#' @param Treat A vector of treatment indicator for each arm. Treatments should have positive integer values starting from 1 to total number of treatments. In a study, lowest number is taken as the baseline treatment. Also, in each study first arm should be the baseline treatment and lower number treatment arms should come before the higher ones. (i.e. treatment arm should go 1, 2, 4 and not 1, 4, 2). See example data for clarification.
 #' @param N A vector of total number of observations in each arm. Used for binomial and multinomial responses.
 #' @param SE A vector of standard error for each arm. Used only for normal response.
 #' @param response Specification of the outcomes type. Must specify one of the following: "normal", "binomial", or "multinomial".
@@ -62,7 +62,7 @@ nodesplit.network.data <- function(Outcomes, Study, Treat,  N = NULL, SE = NULL,
     r <- r[,,1]
   }
   
-  network <- list(Outcomes = Outcomes, Study = Study, Treat = Treat, r = r, t = t, type = type, rank.preference = NULL, nstudy = nstudy, na = na, ntreat = ntreat, b.id = b.id, b.Treat = b.Treat response = response)
+  network <- list(Outcomes = Outcomes, Study = Study, Treat = Treat, r = r, t = t, type = type, rank.preference = NULL, nstudy = nstudy, na = na, ntreat = ntreat, b.id = b.id, response = response)
   
   if(response == "binomial" || response == "multinomial"){
     network$N = N
@@ -72,8 +72,8 @@ nodesplit.network.data <- function(Outcomes, Study, Treat,  N = NULL, SE = NULL,
     network$se = se
   }
   
-  #code <- nodesplit.network.rjags(network)
-  #network$code <- code
+  code <- nodesplit.network.rjags(network)
+  network$code <- code
   
   class(network) <- "nodesplit.network.data"
   return(network)
@@ -97,79 +97,50 @@ nodesplit.binomial.rjags <- function(network){
   
   with(network, {
     
-    code <- paste0("model\n",
-                   "\nfor(i in 1:", nstudy, ") {",
-                   "\n\tw[i,1] <- 0",
-                   "\n\tj[i,1] <- 0",
-                   "\n\tdelta[i,bi[i]] <- 0",
-                   "\n\tmu[i] ~ dnorm(0,.0001)",
-                   "\n\tfor (k in 1:na[i]) {",
-                   "\n\t\tr[i,k] ~ dbin(p[i,t[i,k]], n[i,k])",
-                   "\n\t\tlogit(p[i,t[i,k]]) <- mu[i] + delta[i,t[i,k]]",
-                   "\n\t\tindex[i,k] <- split[i] * (equals(t[i,k], pair[1]) "
-                   
-                   
-                   r[i,k] ~ dbin(p[i,t[i,k]],n[i,k])                   # binomial likelihood
-                   logit(p[i,t[i,k]])<-mu[i] + delta[i,t[i,k]]         # model
-                   index[i,k] <- split[i] * (equals(t[i,k], pair[1]) + equals(t[i,k], pair[2]))
-                   #Deviance contribution
-                   rhat[i,k] <- p[i,t[i,k]] * n[i,k]                   # expected value of the numerators
-                   dev[i,k] <- 2 * (r[i,k] * (log(r[i,k])-log(rhat[i,k]))
-                                    + (n[i,k]-r[i,k]) * (log(n[i,k]-r[i,k]) - log(n[i,k]-rhat[i,k])))
-  }
-                   
-                   
-                   
-                   # w[i,1] <-0
-                   # j[i,1] <-0
-                   # delta[i,bi[i]] <- 0
-                   # mu[i] ~ dnorm(0,.0001)                                    # vague priors for 24 trial baselines
-                   # for (k in 1:na[i])  {
-                   
-                   
-                   )
-    
-    code <- paste0("model\n{",
+    code <- paste0("model{",
                    "\n\tfor(i in 1:", nstudy, ") {",
+                   "\n\t\tw[i,1] <- 0",
+                   "\n\t\tj[i,1] <- 0",
                    "\n\t\tdelta[i,1] <- 0",
-                   "\n\t\tmu[i] ~ dnorm(mean.mu,prec.mu)",
-                   "\n\t\tfor(k in 1:na[i]) {",
-                   "\n\t\t\tr[i,k] ~ dbin(p[i,k], n[i,k])")
-    
-    if(type == "fixed"){
-      code <- paste0(code, "\n\t\t\tlogit(p[i,k]) <- mu[i] + d[t[i,1], t[i,k]]")
-    } else if(type == "random"){
-      code <- paste0(code, "\n\t\t\tlogit(p[i,k]) <- mu[i] + delta[i,k]")
-    }
-    
-    code <- paste0(code,          
-                   "\n\t\t\trhat[i,k] <- p[i,k] * n[i,k]",
-                   "\n\t\t\tdev[i,k] <- 2 * (r[i,k] * (log(r[i,k])- log(rhat[i,k])) + (n[i,k] - r[i,k]) * (log(n[i,k] - r[i,k]) - log(n[i,k] - rhat[i,k])))",
+                   "\n\t\tmu[i] ~ dnorm(0,.0001)",
+                   "\n\t\tfor (k in 1:na[i]) {",
+                   "\n\t\t\tr[i,k] ~ dbin(p[i,t[i,k]], n[i,k])",
+                   "\n\t\t\tlogit(p[i,t[i,k]]) <- mu[i] + delta[i,t[i,k]]",
+                   "\n\t\t\tindex[i,k] <- split[i] * (equals(t[i,k], pair[1]) + equals(t[i,k], pair[2]))",
+                   "\n\t\t\trhat[i,k] <- p[i,t[i,k]] * n[i,k]",
+                   "\n\t\t\tdev[i,k] <- 2 * (r[i,k] * (log(r[i,k])-log(rhat[i,k]))
+                          + (n[i,k]-r[i,k]) * (log(n[i,k]-r[i,k]) - log(n[i,k]-rhat[i,k])))",
                    "\n\t\t}",
-                   "\n\t\tresdev[i] <- sum(dev[i,1:na[i]])"
-    )
-    
-    if(type == "random"){
-      code <- paste0(code, "\n\t\tfor (k in 2:na[i]) {",
-                     "\n\t\t\tdelta[i,k] ~ dnorm(d[t[i,1],t[i,k]], prec)",
-                     "\n\t\t}")
-    }
-    
-    code <- paste0(code, 
+                   "\n\t\tresdev[i] <- sum(dev[i,1:na[i]])",
+                   "\n\t\tfor(k in 2:na[i]){",
+                   "\n\t\t\tdelta[i,t[i,k]] ~ dnorm(md[i,t[i,k]], taud[i,t[i,k]])",
+                   "\n\t\t\t# mean of LOR distritbutions, split into direct and indirect",
+                   "\n\t\t\tmd[i,t[i,k]] <- (d[t[i,k]] - d[t[i,1]]+sw[i,k]) * (1- index[i,k]) + direct * index[i,k]",
+                   "\n\t\t\t# adjustment for multi-arm RCTs with correction for arms removed to split node",
+                   "\n\t\t\tj[i,k] <- k - (equals(1, split[i]) * step(k-3))",
+                   "\n\t\t\ttaud[i, t[i,k]] <- tau * 2 * (j[i,k] -1)/ j[i,k]",
+                   "\n\t\t\tw[i,k] <- (delta[i, t[i,k]] - d[t[i,k]] + d[t[i,1]]) * (1- index[i,k])",
+                   "\n\t\t\tsw[i,k] <- sum(w[i,1:(k-1)])/ (j[i,k] - 1)",
+                   "\n\t\t}",
                    "\n\t}",
-                   "\n\ttotresdev <- sum(resdev[])")
-    
-    code <- paste0(code,
-                   "\n\tfor(c in 1:", ntreat -1, ") {",
-                   "\n\t\tfor(k in (c+1):", ntreat, ") {",
-                   "\n\t\t\td[c,k] ~ dnorm(mean.d, prec.d)",
+                   "\n\td[1] <- 0",
+                   "\n\tdirect ~ dnorm(0, .0001)",
+                   "\n\tfor (k in 2:nt) { d[k] ~ dnorm(0, .0001)}",
+                   "\n\tsd ~ dunif(0,10)",
+                   "\n\tvar <- pow(sd, 2)",
+                   "\n\ttau <- 1/var",
+                   "\n\ttotresdev <- sum(resdev[])",
+                   "\n\tfor (c in 1:", ntreat-1, ") {",
+                   "\n\t\tfor(k in (c+1):nt) {",
+                   "\n\t\t\tor[c,k] <- exp(d[k] - d[c])",
+                   "\n\t\t\tlor[c,k] <- d[k] - d[c]",
                    "\n\t\t}",
-                   "\n\t}")
-    
-    if(type == "random"){
-      code <- paste0(code, ume.hy.prior.rjags(hy.prior, 0))
-    }
-    
+                   "\n\t}",
+                   "\n\tdiff <- direct - lor[pair[1], pair[2]]",
+                   "\n\t#calculate p-value",
+                   "\n\tprob <- step(diff)")
+
+      
     code <- paste0(code, "\n}")
     return(code)
   })
